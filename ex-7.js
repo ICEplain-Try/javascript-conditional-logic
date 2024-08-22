5d034ece79f5a37e4e4ed187407e87968eeb7d02
ICE K. <info.icejourney@gmail.com>
2024-08-22 15:34:20 +0700

    update: EX1-7

--- ex-7.js
+++ ex-7.js
@@ -1,6 +1,13 @@
 //Start coding here
-const greeting = "Hello ! 😁";
 
+// เปลี่ยน greeting จาก variable ให้เป็น function
+function greeting() {
+    console.log("Hello ! 😁");
+};
+
+// ทดสอบ function
 greeting();
-
+greeting();
 console.log("This line will be executed without any error.");
+
+
